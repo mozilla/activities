deb2053051d5d0ae84aaaf660e11508dd72fdb26
mixedpuppy <mixedpuppy@gmail.com>
2012-01-30 17:33:11 -0800

    make twitter close on success

--- data/fakers/twitter.js
+++ data/fakers/twitter.js
@@ -2,7 +2,14 @@
 self.port.on("activity", function(activity) {
   let loc = "https://twitter.com/intent/tweet?url="+activity.data+"&via=Firefox";
   if (loc != unsafeWindow.location) {
-    console.log("updating location "+window.location)
     unsafeWindow.location = loc;
   }
 });
+
+
+if (unsafeWindow.location.pathname == "/intent/tweet/complete") {
+  let result = "https://twitter.com/"+$('.tweet-complete a.launch').attr('href');
+  self.port.emit('activity.result', {
+    data: result
+  });
+}
